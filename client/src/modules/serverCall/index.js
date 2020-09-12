117548db5e7330601e89ddd2b71b726b8830de46
Rajnil Prasad <rajnilprasad.rp@gmail.com>
2020-09-12 09:30:29 -0700

    initial commit

--- client/src/modules/serverCall/index.js
+++ client/src/modules/serverCall/index.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 import qs from 'qs'
 import paypalConfig from '../../configs/paypalConfig'
 
-const URL = 'https://zack-ecommerce-nodejs.herokuapp.com'
+const URL = 'https://stormy-badlands-76842.herokuapp.com/'
 // const URL = 'http://localhost:4000'
 
 const serverCall = (config) => {
